0092176298b3b739170156d2196377ab76ecab7d
Programador Novato <info@programadornovato.com>
2019-06-03 15:41:22 -0500

    27.-Vue js 2 tutorial español 👀 [bootstrap-vue formularios]👀👀

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,15 +1,29 @@
 <template>
     <div>
         <h1>Fromulario</h1>
+        <form>
+            <b-row>
+                <b-col cols="12">
+                    <b-form-input type="text" v-model="texto" :state="valida" ></b-form-input>
+                    {{texto}}
+                </b-col>
+            </b-row>
+        </form>
     </div>
 </template>
 
 <script>
+import { truncate } from 'fs';
 export default {
     name:'form',
     data(){
         return{
-
+            texto:''
+        }
+    },
+    computed:{
+        valida(){
+            return this.texto.length > 5 ? true : false
         }
     }
 }
